81195fc7304fc56d4cb3440e48d3fdd804b87ae0
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2022-12-09 16:15:29 -0800

    fix nextjs.config

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+module.exports = withNextra({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
---
+++ docs/theme.config.jsx
@@ -0,0 +1,6 @@
+export default {
+  logo: <span>SolanaJS</span>,
+  project: {
+    link: 'https://github.com/mustafaaljadery/solanajs',
+  },
+};
